8e808f9c80a508d60e71cadf5bd6552c08a8ce43
Rui Silva <115831206+ruidanielGit@users.noreply.github.com>
2026-08-14 09:33:41 +0100

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<String, Integer>  wordCountMapS = getCharCountMap(s);
+        HashMap<String, Integer>  wordCountMapT = getCharCountMap(t);
+
+        if(wordCountMapS.equals(wordCountMapT)){
+            return true;
+        }
+
+    return false;
+    }
+
+    public HashMap<String, Integer> getCharCountMap (String word){
+
+        HashMap<String, Integer>  charCountMap = new HashMap<String, Integer>();
+
+        for(int i=0; i < word.length(); i++){
+            String theChar = String.valueOf(word.charAt(i));
+            boolean existsInMap = charCountMap.containsKey(theChar);
+
+            if(existsInMap){
+                int numOfOccurencies = charCountMap.get(theChar);
+                charCountMap.put(theChar, numOfOccurencies + 1);
+            } else {
+                charCountMap.put(theChar, 1);
+            }
+        }
+
+        charCountMap.forEach( (k, v) -> { System.out.println(k + " -> " + v); } );
+
+        return charCountMap;
+    }
+
+}
